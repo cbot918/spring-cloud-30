14c22e62aaba0fe6630c7249bf6f559498fa913b
nodev918 <devnode77@gmail.com>
2024-03-18 15:35:07 +0800

    update: sc > add Spring-Data-Rest

--- sc/src/main/java/com/example/sc/repository/BookRepository.java
+++ sc/src/main/java/com/example/sc/repository/BookRepository.java
@@ -1,10 +1,32 @@
 package com.example.sc.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.example.sc.model.Book;
 
-@Repository
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.Optional;
+
+@RepositoryRestResource
 public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    // Prevents GET /books/:id
+    @Override
+    Optional<Book> findById(Integer id);
+
+    // Prevents GET /books
+    @Override
+    Page<Book> findAll(Pageable pageable);
+
+    // Prevents POST /books and PATCH /books/:id
+    @Override
+    Book save(Book s);
+
+    // Prevents DELETE /books/:id
+    @Override
+    @RestResource(exported = false)
+    void delete(Book t);
 }
